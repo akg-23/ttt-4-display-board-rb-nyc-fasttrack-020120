8c00acfeae85d8c6b1d541219c9e103dc66c1a82
Aaron Gyapong <aaronkg23@gmail.com>
2020-02-10 23:35:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,114 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-the_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" "," "," "," "," "," "," "," "," "]
 
-
-
-def display_board(the_board)
-  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
+def display_board(board)
+  puts "#{board[0]} | #{board[1]} | #{board[2]}  "
   puts "___________"
-  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
+  puts "#{board[3]} | #{board[4]} | #{board[5]}  "
   puts "___________"
-  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
+  puts "#{board[6]} | #{board[7]} | #{board[8]}  "
 end
-
-display_board(the_board)
-
-turn_1 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-def display_board(turn_1)
-  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
-  puts "___________"
-  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
-  puts "___________"
-  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
-end
-
-display_board(the_board)
-
-
-
-
-
-the_board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(the_board)
-  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
-  puts "___________"
-  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
-  puts "___________"
-  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
-end
-
-display_board(the_board)
-
-the_board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-
-def display_board(the_board)
-  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
-  puts "___________"
-  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
-  puts "___________"
-  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
-end
-display_board(the_board)
-
-the_board = ["O ", " ", " ", " ", "X ", " ", " ", " ", " "]
-def display_board(the_board)
-  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
-  puts "___________"
-  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
-  puts "___________"
-  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
-end
-display_board(the_board)
-
-turn_5 = ["X ", "X ", "X ", " ", " ", " ", " ", " ", " "]
-def display_board(the_board)
-  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
-  puts "___________"
-  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
-  puts "___________"
-  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
-end
-
-display_board(the_board)
-
-the_board = [" ", " ", " ", " ", " ", " ", "O ", "O ", "O "]
-
-def display_board(the_board)
-  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
-  puts "___________"
-  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
-  puts "___________"
-  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
-end
-display_board(the_board)
-
-turn_7 = ["X ", " ", " ", " ", "X ", " ", " ", " ", "X "]
-def display_board(the_board)
-  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
-  puts "___________"
-  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
-  puts "___________"
-  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
-end
-display_board(the_board)
-
-the_board = [" ", " ", "O ", " ", "O ", " ", "O ", " ", " "]
-def display_board(the_board)
-  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
-  puts "___________"
-  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
-  puts "___________"
-  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
-end
-display_board(the_board)
-
-the_board = ["X ", "X ", "X ", "X ", "X ", "X ", " ", " ", " "]
-def display_board(the_board)
-  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
-  puts "___________"
-  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
-  puts "___________"
-  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
-end
- display_board(the_board)
+display_board(board)
