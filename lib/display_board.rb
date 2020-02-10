d026bc3962c42606face2407b88123975a0e9dd0
Aaron Gyapong <aaronkg23@gmail.com>
2020-02-10 22:04:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-turn_1 = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+
 
 def display_board(the_board)
   puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
@@ -12,4 +12,102 @@ def display_board(the_board)
   puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
 end
 
+display_board(the_board)
+
+turn_1 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+def display_board(turn_1)
+  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
+  puts "___________"
+  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
+  puts "___________"
+  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
+end
+
 display_board(turn_1)
+
+
+
+
+
+turn_2 = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(turn_2)
+  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
+  puts "___________"
+  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
+  puts "___________"
+  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
+end
+
+display_board(turn_2)
+
+turn_3 = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(turn_3)
+  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
+  puts "___________"
+  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
+  puts "___________"
+  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
+end
+
+turn_4 = ["O ", " ", " ", " ", "X ", " ", " ", " ", " "]
+def display_board(turn_4)
+  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
+  puts "___________"
+  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
+  puts "___________"
+  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
+end
+display_board(turn_4)
+
+turn_5 = ["X ", "X ", "X ", " ", " ", " ", " ", " ", " "]
+def display_board(turn_4)
+  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
+  puts "___________"
+  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
+  puts "___________"
+  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
+end
+
+display_board(turn_5)
+
+turn_6 = [" ", " ", " ", " ", " ", " ", "O ", "O ", "O "]
+
+def display_board(turn_6)
+  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
+  puts "___________"
+  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
+  puts "___________"
+  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
+end
+display_board(turn_6)
+
+turn_7 = ["X ", " ", " ", " ", "X ", " ", " ", " ", "X "]
+def display_board(turn_7)
+  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
+  puts "___________"
+  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
+  puts "___________"
+  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
+end
+display_board(turn_7)
+
+turn_8 = [" ", " ", "O ", " ", "O ", " ", "O ", " ", " "]
+def display_board(turn_7)
+  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
+  puts "___________"
+  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
+  puts "___________"
+  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
+end
+display_board(turn_8)
+
+turn_9 = ["X ", "X ", "X ", " ", " ", " ", " ", " ", " "]
+def display_board(turn_9)
+  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
+  puts "___________"
+  puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
+  puts "___________"
+  puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
+end
+ display_board(turn_9)
